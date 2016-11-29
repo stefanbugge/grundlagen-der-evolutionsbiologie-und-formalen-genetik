9eb47f85c48547f4c25f565e0483ab19b7bbfbba
Stefan Loeschcke <stefan@loeschcke.dk>
2016-11-29 17:02:35 +0100

    site as static content for now

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import 'babel-polyfill'
 import React from 'react';
 import { render } from 'react-dom';
 
+import "./app/containers/app.less";
+
 import configureStore from './app/store/configureStore';
 import Root from './app/containers/Root';
 
 const store = configureStore({});
 
-render(
-  <Root store={store} />,
-  document.getElementById('app-container')
-);
-
-console.log('foo')
+// render(
+//   <Root store={store} />,
+//   document.getElementById('app-container')
+// );
